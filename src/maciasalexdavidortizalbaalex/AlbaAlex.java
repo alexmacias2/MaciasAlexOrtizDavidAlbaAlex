a1e17627fb4d453f48ab8ba5e1092747874813bc
alexm <alexm@DESKTOP-MJ0D4HD>
2018-10-07 14:12:40 +0200

    Commit alejandro

---
+++ src/maciasalexdavidortizalbaalex/AlbaAlex.java
@@ -0,0 +1,20 @@
+package maciasalexdavidortizalbaalex;
+
+/**
+ *
+ * @author aalexal
+ */
+public class AlbaAlex {
+
+    public void AlbaAlex() {
+        System.out.println("Hola soy Alex Alba y añado código en mi clase");
+    } 
+    
+    public void OrtizDavid() {
+    System.out.println("Codigo David clase Alex");
+    } 
+    
+    public void MaciasAlejandro() {
+    System.out.println("Añado código a mi método de la clase de Alex Alba");
+    }
+}
---
+++ src/maciasalexdavidortizalbaalex/DavidOrtiz.java
@@ -0,0 +1,21 @@
+package maciasalexdavidortizalbaalex;
+
+/**
+ *
+ * @author alexm
+ */
+public class DavidOrtiz {
+
+    public void david1() {
+        System.out.println("Cambio codigo de este método otra vez yo alejandro");
+    }
+    
+    public void david2() {
+          System.out.println("Codigo Alejandro Macias clase david");
+    }
+    
+    public void david3() {
+        System.out.println("Hola soy Alex Alba y añado código en clase David");
+    }
+    
+}
