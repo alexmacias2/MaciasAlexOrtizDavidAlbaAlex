152bf36b7d477a4d99c8e301c5b8aaccdd396a12
Zentyx <cmortizdavid@gmail.com>
2018-10-07 12:18:03 +0200

    Create DavidOrtiz.java

---
+++ src/maciasalexdavidortizalbaalex/DavidOrtiz.java
@@ -0,0 +1,21 @@
+package maciasalexdavidortizalbaalex;
+
+/**
+ *
+ * @author alexm
+ */
+public class DavidOrtiz {
+
+    public void david1() {
+    
+    }
+    
+    public void david2() {
+    
+    }
+    
+    public void david3() {
+    
+    }
+    
+}
