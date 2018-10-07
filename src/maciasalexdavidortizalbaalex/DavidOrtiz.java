36ea7e6e8e8cb2242532216788e3c2e716a373eb
David <David@LAPTOP-66DVRHG7>
2018-10-07 14:01:55 +0200

    Commit David2

--- src/maciasalexdavidortizalbaalex/DavidOrtiz.java
+++ src/maciasalexdavidortizalbaalex/DavidOrtiz.java
@@ -7,7 +7,7 @@ package maciasalexdavidortizalbaalex;
 public class DavidOrtiz {
 
     public void david1() {
-        System.out.println("Cambio codigo de este método");
+        System.out.println("Cambio codigo de este método otra vez");
     }
     
     public void david2() {
