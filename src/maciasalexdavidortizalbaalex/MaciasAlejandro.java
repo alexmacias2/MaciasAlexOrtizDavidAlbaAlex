6b9fbc6ae456792caa6fc962db850144506d6636
alexmacias2 <43613826+alexmacias2@users.noreply.github.com>
2018-10-07 12:15:51 +0200

    Add files via upload
    Clase Alejandro Macias

---
+++ src/maciasalexdavidortizalbaalex/MaciasAlejandro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maciasalexdavidortizalbaalex;
+
+/**
+ *
+ * @author alexm
+ */
+public class MaciasAlejandro {
+
+    public void OrtizDavid() {
+
+    } public void AlbaAlex() {
+
+    } public void MaciasAlejandro() {
+
+    }
+
+}
