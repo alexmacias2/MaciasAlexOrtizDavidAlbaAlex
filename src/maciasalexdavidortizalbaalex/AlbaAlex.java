ff2f12028875389dbd28b3d08100354513366e6e
aalexal <43817817+aalexal@users.noreply.github.com>
2018-10-07 12:23:03 +0200

    Create AlbaAlex.java

---
+++ src/maciasalexdavidortizalbaalex/AlbaAlex.java
@@ -0,0 +1,20 @@
+package maciasalexdavidortizalbaalex;
+
+/**
+ *
+ * @author aalexal
+ */
+public class AlbaAlex {
+
+    public void AlbaAlex() {
+
+    } 
+    
+    public void OrtizDavid() {
+
+    } 
+    
+    public void MaciasAlejandro() {
+
+    }
+}
