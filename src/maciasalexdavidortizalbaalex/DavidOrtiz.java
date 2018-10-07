d24e61de3338639e6724bbd8be3c6fe61c68ec97
LAPTOP-NTCGK8E9\alexa <alexalba123@gmail.com>
2018-10-07 18:31:12 +0200

    Commit Alex

---
+++ src/maciasalexdavidortizalbaalex/DavidOrtiz.java
@@ -0,0 +1,21 @@
+package maciasalexdavidortizalbaalex;
+
+/**
+ *
+ * @author alexm
+ */
+public class DavidOrtiz {
+
+    public void david1() {
+        System.out.println("Soy David Genero Conflicto");
+    }
+    
+    public void david2() {
+          System.out.println("Codigo Alejandro Macias clase david");
+    }
+    
+    public void david3() {
+        System.out.println("Hola soy Alex Alba y añado código en clase David");
+    }
+    
+}
---
+++ src/maciasalexdavidortizalbaalex/MaciasAlejandro.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maciasalexdavidortizalbaalex;
+
+/**
+ *
+ * @author alexm
+ */
+public class MaciasAlejandro {
+
+    public void OrtizDavid() {
+        System.out.println("Codigo David clase Alejandro");
+    } 
+    public void AlbaAlex() {
+        System.out.println("Hola soy Alex Alba y añado código en clase Alejandro");
+    } 
+    public void MaciasAlejandro() {
+        System.out.println("Hola añado código");
+    }
+    
+
+}
